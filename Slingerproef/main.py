0162f5b5d5e7c13923d542adfcdb4e2a02129066
Tein van der Lugt <teinvdlugt.developer@gmail.com>
2018-02-22 13:31:56 +0100

    asfdkl

--- Slingerproef/main.py
+++ Slingerproef/main.py
@@ -14,15 +14,17 @@ xdata = np.take(data, [0], 1) / 100
 
 # De ydata is de periode van de slinger T. Omrekenen van tijd van 10 slingeringen naar tijd van 1 slingering.
 ydata = np.take(data, [1], 1) / 10
+# De yerr is de fout in T gedeeld door 10
+yerr = np.take(data, [3], 1) / 10
 
 print(np.multiply(np.sqrt(np.divide(xdata, ydata)), 2 * pi)[:, 0])
 
 
 def formula(l, g):
-    return np.multiply(np.sqrt(np.divide(l, g)), 2 * pi)[:, 0]
+    return np.multiply(np.sqrt(np.divide(l, g)), 2 * pi)
 
 
-popt, pcov = opt.curve_fit(formula, xdata, ydata)
+popt, pcov = opt.curve_fit(formula, xdata.ravel(), ydata.ravel(), sigma=yerr.ravel(), absolute_sigma=True)  # TODO yerr werkt niet?
 
 print(popt)
 print(pcov)
